a0596f16965748fe4347e3a41d014d6ce0b83e6d
Howard P <prioleau1123@gmail.com>
2025-01-22 21:39:15 -0500

    assignment1

---
+++ LSP_REPO_JavaTheHut/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	/**
+	*  Name: Howard Prioleau
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
